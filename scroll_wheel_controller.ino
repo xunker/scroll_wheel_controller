1c53a78249f9b506ec34e399fc7643f6edf99b15
Matthew Nielsen <xunker@pyxidis.org>
2019-05-19 22:41:36 -0600

    Working with oled display

--- scroll_wheel_controller.ino
+++ scroll_wheel_controller.ino
@@ -8,7 +8,20 @@
 // Be sure to enable half-step mode
 #include <Rotary.h>  // https://github.com/brianlow/Rotary
 
-#define USE_SERIAL
+// Assume 128x64 oled is connected via I2C
+// https://github.com/greiman/SSD1306Ascii
+#include <Wire.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h"
+// 0X3C+SA0 - 0x3C or 0x3D
+#define I2C_ADDRESS 0x3C
+// Define proper RST_PIN if required.
+#define RST_PIN -1
+SSD1306AsciiWire oled;
+uint8_t displayHeightInRows;
+uint8_t displayWidthInColumns;
+
+// #define USE_SERIAL
 #ifdef USE_SERIAL
   #define debug(msg) Serial.print(msg)
   #define debugln(msg) Serial.println(msg)
@@ -105,39 +118,39 @@ struct controlMode {
 #define NUMBER_OF_MODES 5
 controlMode controlModeList[NUMBER_OF_MODES] = {
     {{"Volume"},
-     {"", NULL, NULL, NULL, NULL},
-     {"", NULL, NULL, NULL, NULL},
-     {"Vol -", NULL, NULL, NULL, MEDIA_VOLUME_DOWN},
+     {"--", NULL, NULL, NULL, NULL},
+     {"--", NULL, NULL, NULL, NULL},
+     {"- Vol", NULL, NULL, NULL, MEDIA_VOLUME_DOWN},
      {"Vol +", NULL, NULL, NULL, MEDIA_VOLUME_UP},
      {"Mute", NULL, NULL, NULL, MEDIA_VOLUME_MUTE}},
 
     {{"Media"},
-     {"Previous", NULL, NULL, NULL, MEDIA_PREVIOUS},
-     {"Next", NULL, NULL, NULL, MEDIA_NEXT},
-     {"Seek <", NULL, NULL, NULL, MEDIA_REWIND, LONG_KEY_DOWN_TIME},
+     {"Prev\nTrack", NULL, NULL, NULL, MEDIA_PREVIOUS},
+     {"Next\nTrack", NULL, NULL, NULL, MEDIA_NEXT},
+     {"< Seek", NULL, NULL, NULL, MEDIA_REWIND, LONG_KEY_DOWN_TIME},
      {"Seek >", NULL, NULL, NULL, MEDIA_FAST_FORWARD, LONG_KEY_DOWN_TIME},
-     {"Play/Pause", NULL, NULL, NULL, MEDIA_PLAY_PAUSE}},
+     {"Play/\nPause", NULL, NULL, NULL, MEDIA_PLAY_PAUSE}},
 
     {{"VLC"},
-     {"Previous", KEY_LEFT_GUI, KEY_LEFT_ARROW, NULL, NULL},
-     {"Next", KEY_LEFT_GUI, KEY_RIGHT_ARROW, NULL, NULL},
-     {"Scrub <", KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ARROW, NULL},
+     {"Prev\nTrack", KEY_LEFT_GUI, KEY_LEFT_ARROW, NULL, NULL},
+     {"Next\nTrack", KEY_LEFT_GUI, KEY_RIGHT_ARROW, NULL, NULL},
+     {"< Scrub", KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ARROW, NULL},
      {"Scrub >", KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_RIGHT_ARROW, NULL},
-     {"Play/Pause", KEY_SPACE, NULL, NULL, NULL}},
+     {"Play/\nPause", KEY_SPACE, NULL, NULL, NULL}},
 
     {{"Mouse"},
-     {"", NULL, NULL, NULL, NULL},
-     {"", NULL, NULL, NULL, NULL},
-     {"Up", NULL, NULL, NULL, NULL, MOUSE_SCROLL_NEGATIVE},
-     {"Down", NULL, NULL, NULL, NULL, MOUSE_SCROLL_POSITIVE},
-     {"", NULL, NULL, NULL, NULL}},
+     {"--", NULL, NULL, NULL, NULL},
+     {"--", NULL, NULL, NULL, NULL},
+     {"^ Scroll", NULL, NULL, NULL, NULL, MOUSE_SCROLL_NEGATIVE},
+     {"Scroll v", NULL, NULL, NULL, NULL, MOUSE_SCROLL_POSITIVE},
+     {"--", NULL, NULL, NULL, NULL}},
 
     {{"Navigation"},
-     {"Back", KEY_LEFT_GUI, KEY_LEFT_BRACE, NULL, NULL},     // CONSUMER_BROWSER_BACK maybe
-     {"Forward", KEY_LEFT_GUI, KEY_RIGHT_BRACE, NULL, NULL}, // CONSUMER_BROWSER_FORWARD maybe
-     {"Up", KEY_UP_ARROW, NULL, NULL, NULL},
-     {"Down", KEY_DOWN_ARROW, NULL, NULL, NULL},
-     {"Select", KEY_ENTER, NULL, NULL, NULL}}};
+     {"Next\nPage", KEY_LEFT_GUI, KEY_LEFT_BRACE, NULL, NULL},     // CONSUMER_BROWSER_BACK maybe
+     {"Prev\nPage", KEY_LEFT_GUI, KEY_RIGHT_BRACE, NULL, NULL}, // CONSUMER_BROWSER_FORWARD maybe
+     {"^ Arrow", KEY_UP_ARROW, NULL, NULL, NULL},
+     {"Arrow v", KEY_DOWN_ARROW, NULL, NULL, NULL},
+     {"Enter", KEY_ENTER, NULL, NULL, NULL}}};
 
 uint8_t currentModeIndex = 0;
 
@@ -154,11 +167,143 @@ controlMode toggleMode() {
   return controlModeList[toggleModeIndex];
 }
 
+controlMode previousMode() {
+  return controlModeList[previousModeIndex];
+}
+
+// Print to oled left-justified with newline support
+void oledPrintLeftJustify(String msg, uint8_t row)
+{
+  String substr = "";
+  for (uint8_t i = 0; i < msg.length(); i++) {
+    if (msg[i] == '\n') {
+      oled.setCursor(0, row);
+      oled.print(substr);
+      substr = "";
+      row++;
+    } else {
+      substr = substr + msg[i];
+    }
+  }
+  if (substr.length() > 0) {
+    oled.setCursor(0, row);
+    oled.print(substr);
+  }
+}
+
+// Print to oled right-justified with newline support
+void oledPrintRightJustify(String msg, uint8_t row) {
+  String substr = "";
+  for (uint8_t i = 0; i < msg.length(); i++) {
+    if (msg[i] == '\n') {
+      oled.setCursor(((oled.displayWidth() - oled.fieldWidth(substr.length()))), row);
+      oled.print(substr);
+      substr = "";
+      row++;
+    } else {
+      substr = substr + msg[i];
+    }
+  }
+  if (substr.length() > 0) {
+    oled.setCursor(((oled.displayWidth() - oled.fieldWidth(substr.length()))), row);
+    oled.print(substr);
+  }
+}
+
+// Print to oled centered with newline support
+void oledPrintCentered(String msg, uint8_t row) {
+  String substr = "";
+  for (uint8_t i = 0; i < msg.length(); i++) {
+    if (msg[i] == '\n') {
+      oled.setCursor(((oled.displayWidth() - oled.fieldWidth(substr.length()))/2), row);
+      oled.print(substr);
+      substr = "";
+      row++;
+    } else {
+      substr = substr + msg[i];
+    }
+  }
+  if (substr.length() > 0) {
+    oled.setCursor(((oled.displayWidth() - oled.fieldWidth(substr.length()))/2), row);
+    oled.print(substr);
+  }
+}
+
+// Update the connected oled display
+void updateDisplay() {
+  oled.clear();
+
+  oledPrintCentered(currentMode().name + " Mode", 0);
+
+  // oled.setCursor(0, 1);
+  // oled.print("01234567890123456789012345678901234567890");
+
+  // middle button
+  oledPrintCentered(currentMode().middle.name, 2);
+
+  // left button
+  oledPrintLeftJustify(currentMode().left.name, 2);
+
+  // right button
+  oledPrintRightJustify(currentMode().right.name, 2);
+
+  // wheel actions
+  oledPrintCentered((currentMode().wheelCW.name +  " .. " + currentMode().wheelCCW.name), displayHeightInRows - 3);
+
+  // mode quick-toggle
+  if (previousModeIndex == currentModeIndex) {
+    if (currentModeIndex != toggleModeIndex) {
+      oledPrintCentered(toggleMode().name + " ->", displayHeightInRows - 1);
+    }
+  } else {
+    if (previousModeIndex != toggleModeIndex) {
+      oledPrintCentered("<- " + previousMode().name, displayHeightInRows - 1);
+    } else {
+      oledPrintCentered(toggleMode().name + " ->", displayHeightInRows - 1);
+    }
+  }
+}
+
 void setup() {
-  #ifdef USE_SERIAL
+  // #ifdef USE_SERIAL
+    // leave serial enabled so we can reprogram properly
     Serial.begin(9600);
-  #endif
-  delay(5000);
+  // #endif
+
+  Wire.begin();
+  // Wire.setClock(400000L);
+
+  #if RST_PIN >= 0
+    oled.begin(&Adafruit128x64, I2C_ADDRESS, RST_PIN);
+  #else  // RST_PIN >= 0
+    oled.begin(&Adafruit128x64, I2C_ADDRESS);
+  #endif // RST_PIN >= 0
+
+  // oled.setFont(Iain5x7); // proportional
+  // oled.setFont(Adafruit5x7);
+  // oled.setFont(fixed_bold10x15);
+  // oled.setFont(font5x7);
+  // oled.setFont(font8x8);
+  // oled.setFont(lcd5x7);
+  // oled.setFont(newbasic3x5);
+  oled.setFont(Stang5x7);
+  // oled.setFont(System5x7);
+  // oled.setFont(Wendy3x5);
+  // oled.setFont(X11fixed7x14);
+  // oled.setFont(X11fixed7x14B);
+  // oled.setFont(ZevvPeep8x16);
+
+  displayHeightInRows = (oled.displayHeight() / oled.fontHeight()) - 1;
+  displayWidthInColumns = (oled.displayWidth() / oled.fontWidth()) - 1;
+
+  oled.clear();
+  oled.print("Booting");
+  for (uint8_t i = 0; i < 10; i++) {
+    delay(200);
+    oled.print(".");
+  }
+
+  updateDisplay();
 
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LED_BUILTIN_OFF);
@@ -284,6 +429,7 @@ void loop() {
     debug(currentMode().name);
     debugln("'");
 
+    // updateDisplay();
   }
 
   if (leftButton.wasPressed()) {
@@ -307,12 +453,14 @@ void loop() {
     previousModeIndex = currentModeIndex;
 
     changeModeMessage();
+    updateDisplay();
   } else if (downButton.wasPressed()) {
     if (currentModeIndex == toggleModeIndex) {
       if (previousModeIndex != toggleModeIndex) {
         debugln("Returning to previous mode");
         currentModeIndex = previousModeIndex;
         changeModeMessage();
+        updateDisplay();
       } else {
         debugln("Toggle mode and previous mode are the same");
       }
@@ -322,6 +470,7 @@ void loop() {
       debugln("'");
 
       currentModeIndex = toggleModeIndex;
+      updateDisplay();
     }
   }
 
